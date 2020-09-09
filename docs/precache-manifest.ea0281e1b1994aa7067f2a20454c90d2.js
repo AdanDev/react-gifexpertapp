f2ffadae25f97e2a92cc0e2f4b4e4dec16077798
Adan Risueño <adan.ripoll.dev@gmail.com>
2020-09-09 11:56:06 +0200

    docs agregado

---
+++ docs/precache-manifest.ea0281e1b1994aa7067f2a20454c90d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47bb121ecac3b107558697d41709d3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd593216f88ab6c3b7c8",
+    "url": "/static/css/main.885a48c3.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd593216f88ab6c3b7c8",
+    "url": "/static/js/main.529eee4f.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
